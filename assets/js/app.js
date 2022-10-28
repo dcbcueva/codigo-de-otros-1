d483ef96e27761f494a0c5dfc178c4cdc299183d
Diego Bonilla <dcbcueva@gmail.com>
2022-10-28 14:39:31 -0500

    fix:data no definida y queryselector bien declarado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('.name');
-const $b = document.querySelector('.blog');
-const $l = document.querySelector('.location');
+/* querySelector con su p de parrafo */
+const $n = document.querySelector('p.name');
+const $b = document.querySelector('p.blog');
+const $l = document.querySelector('p.location');
 
+/* agregar async para que el await funcione */
 async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  /* Definir data que no estaba definido */
+  const data = await response.json();
   console.log(data);
   $n.textContent = `${data.name}`;
   $b.textContent = `${data.blog}`;
