0e23b2d7cd195f2b096083c24bb3c9a810d87e2e
Diego Bonilla <dcbcueva@gmail.com>
2022-10-28 13:27:40 -0500

    fix:cambios js para etiqueta de css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
 async function displayUser(username) {
